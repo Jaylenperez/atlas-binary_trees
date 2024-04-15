2639b9cee2d78f6dae62d3509c767f117cb7b9a2
jaylenperez <jaylenestella24@gmail.com>
2024-04-15 14:10:22 -0500

    removed trailing whitespace on line 7 and 16

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -4,7 +4,7 @@
  * binary_tree_node - Creates a new binary tree node
  * @parent: Pointer to the parent node of the node to create
  * @value: The value to put in the new node
- * 
+ *
  * Return: Pointer to the new node, or NULL on failure
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
@@ -13,7 +13,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	if (new_node == NULL)
 		return (NULL);
-	
+
 	new_node->n = value;
 	new_node->parent = parent;
 	new_node->left = NULL;
