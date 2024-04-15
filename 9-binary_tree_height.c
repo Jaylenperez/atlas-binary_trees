6c543648d6d2e663516cf15133f4162ea46554e0
jaylenperez <jaylenestella24@gmail.com>
2024-04-15 18:48:38 -0500

    implemented height and depth

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -16,8 +16,5 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
-	if (left_height == 0 && right_height == 0)
-		return (0);
-	else
-		return (left_height > right_height ? left_height : right_height) + 1;
+	return (left_height > right_height ? left_height : right_height) + 1;
 }
