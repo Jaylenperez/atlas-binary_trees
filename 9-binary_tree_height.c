27ad1de66726b90347e5a225c05c45c0e5efa368
jaylenperez <jaylenestella24@gmail.com>
2024-04-15 19:03:41 -0500

    removed trailing whitespace 6, 15, 18, and added parentheses on 22

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_height - Measures the height of a binary tree
  * @tree: Pointer to the root node of the tree to measure the height
- * 
+ *
  * Return: The height of the tree
  */
 size_t binary_tree_height(const binary_tree_t *tree)
@@ -12,12 +12,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	
+
 	if (tree->left == NULL && tree->right == NULL)
 		return (0);
-	
+
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
-	return (left_height > right_height ? left_height : right_height) + 1;
+	return ((left_height > right_height ? left_height : right_height) + 1);
 }
