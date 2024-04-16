c5f589d34c64e07252e1b25b523d242aeb2b55b1
jaylenperez <jaylenestella24@gmail.com>
2024-04-15 20:38:50 -0500

    corrected balance factor calculation

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -12,5 +12,8 @@ int binary_tree_balance(const binary_tree_t *tree)
     if (tree == NULL)
         return (0);
     
-    return ((int)(binary_tree_height(tree->left) - binary_tree_height(tree->right)));
+    size_t left_height = binary_tree_height(tree->left);
+    size_t right_height = binary_tree_height(tree->right);
+    
+    return ((int)(left_height - right_height));
 }
