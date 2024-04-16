29456b97af694e0aa44afc424c279a8ab07fbd62
jaylenperez <jaylenestella24@gmail.com>
2024-04-15 20:04:42 -0500

    removed whitespace on line 6

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_nodes - Counts the nodes with at least 1 child in binary tree
  * @tree: Pointer to the root node of the tree to count the number of nodes
- * 
+ *
  * Return: Number of nodes with at least 1 child, or 0 if tree is NULL
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
