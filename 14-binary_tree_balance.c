630b03c519af96880b39900c6c9a8f3714a4b2c9
jaylenperez <jaylenestella24@gmail.com>
2024-04-15 20:55:03 -0500

    correctly moved size_t to beginning function

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,11 +9,10 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+    size_t left_height, right_height;
+
     if (tree == NULL)
         return (0);
-    
-    size_t left_height;
-    size_t right_height;
     
     left_height = binary_tree_height(tree->left);
     right_height = binary_tree_height(tree->right);
